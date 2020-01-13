59d78de7d1462d73badd11a8843c446cfcd890e3
leojr <leo.junior.ribeiro@gmail.com>
2020-01-13 10:08:32 -0300

    Teste usando intellij especificando brench

--- sitebusca/src/main/java/email/Teste2.java
+++ sitebusca/src/main/java/email/Teste2.java
@@ -8,11 +8,10 @@ public  class Teste2 {
 
     public static void teste2() {
         Teste2 teste2 = new Teste2();
+        System.out.println("test");
     }
 
-
     public static void main(String[] args) {
-
         teste2();
     }
 
